f6c47719fccd8ca8109ef03691bccd9c77426179
PHUC LE <lqtpstudy@gmail.com>
2023-12-27 17:07:17 +0700

    Add danhmuc operations

--- src/BLL/danhmucDAO.java
+++ src/BLL/danhmucDAO.java
@@ -33,4 +33,46 @@ public class danhmucDAO extends Ket_Noi_CSDL {
         }
         return list;
     }
+
+    public ArrayList<danhmuc> themdanhmuc(String id, String ten) {
+        try {
+            string sql = "insert into danhmuc values (?, ?, 0)";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, id);
+            preparedStatement.setString(2, ten);
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public ArrayList<danhmuc> suadanhmuc(String oldID, String newID, String ten) {
+        try {
+            string sql = "update danhmuc set id = ?, ten = ? where id = ?";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, newID);
+            preparedStatement.setString(2, ten);
+            preparedStatement.setString(3, oldID);
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public ArrayList<danhmuc> xoadanhmuc(String id) {
+        try {
+            string sql = "update danhmuc set xoa = 1 where id = ?";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, id);
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 }
